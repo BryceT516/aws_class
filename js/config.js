1e91310e6c769ccd72f1be9220656ff026f14762
Bryce Tucker <bryce.tucker@hp.com>
2020-01-08 19:29:18 +0000

    Set cognito config settings

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_irqKyho1n', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'brljne8u8jh8fhgjqirnapa5q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
